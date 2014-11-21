ded38e86f03b4f324af44be51db853e741a5b08e
Marcos Ojeda <subliminal@gmail.com>
2014-11-20 21:01:11 -0800

    Initial Commit

---
+++ cliptest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  cliptest
+//
+//  Created by Marcos Ojeda on 11/20/14.
+//  Copyright (c) 2014 Marcos Ojeda. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
